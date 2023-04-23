12550a6378588f394e3c3b5330a4bc0378350acf
Jess Cushnie <94955791+jmcushnie@users.noreply.github.com>
2023-04-23 20:32:59 +1200

    First commit, game window

---
+++ Pong/src/Pong.java
@@ -0,0 +1,29 @@
+import javax.swing.JFrame;
+ 
+public class Pong extends JFrame {
+	private static final int WINDOW_WIDTH = 800;
+    private static final int WINDOW_HEIGHT = 600;
+	
+    public Pong() {
+        	setTitle("Pong");
+setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+        	setResizable(false);
+        	add(new PongPanel());
+        	setVisible(true);
+        	setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+ 
+    public static void main(String[] args) {      
+    	 
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+ 
+          public void run() {
+ 
+                 new Pong();
+ 
+          }
+ 
+        });
+ 
+    }
+}
---
+++ Pong/src/PongPanel.java
@@ -0,0 +1,38 @@
+import javax.swing.JPanel;
+
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class PongPanel extends JPanel implements ActionListener, KeyListener {
+
+	public PongPanel () {
+			setBackground(Color.BLACK);
+	}
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
